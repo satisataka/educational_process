88ee89ea8ebe0d2bf13b10b854cf0b2e22dd939a
satisataka <satisataka@gmail.com>
2023-07-20 17:35:50 +0300

    [fix] SoftDeletedUUIDModel

--- university/common/models.py
+++ university/common/models.py
@@ -6,18 +6,34 @@ from django.utils import timezone
 
 class SoftDeletedManager(models.Manager):
     """
-    Manager will be excluded deleted objects from queryset
+    will be excluded deleted objects from queryset
     """
     def get_queryset(self):
         return super().get_queryset().exclude(deleted_at__isnull=False)
 
 
-class SoftDeleteModelMixin:
+class UUIDModel(models.Model):
     """
-    mixin for soft deleted any Model
-    expected that this mixin will be applied with a SoftDeletedManager
+    default abstract model for custom models
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-created_at', )
+        abstract = True
+
+
+class SoftDeletedUUIDModel(UUIDModel):
+    """
+    default soft delete abstract model with for custom models
     """
     deleted_at = models.DateTimeField(null=True, default=None, db_index=True)
+
+    objects = SoftDeletedManager
+    all_objects = models.Manager
 
     def soft_delete(self):
         self.deleted_at = timezone.now()
@@ -27,26 +43,5 @@ class SoftDeleteModelMixin:
         self.deleted_at = None
         self.save()
 
-
-class UUIDModel(models.Model):
-    """
-    default abstract model for custom models
-    """
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        abstract = True
-
-
-class SoftDeletedUUIDModel(SoftDeleteModelMixin, UUIDModel):
-    """
-    default abstract model with SoftDeleteModelMixin for custom models
-    """
-    objects = SoftDeletedManager
-    all_objects = models.Manager
-
-    class Meta:
+    class Meta(UUIDModel.Meta):
         abstract = True
