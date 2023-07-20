ab88b0996d18d754ec6348ace6a11bb6ee9645dd
satisataka <satisataka@gmail.com>
2023-07-20 17:10:12 +0300

    [fix] default abstract Models

--- university/common/models.py
+++ university/common/models.py
@@ -1,25 +1,51 @@
 import uuid
 
 from django.db import models
+from django.utils import timezone
 
 
-class ExcludeDeletedManagerMixin:
+class SoftDeletedManager(models.Manager):
+    """
+    Manager will be excluded deleted objects from queryset
+    """
     def get_queryset(self):
-        return super().get_queryset().filter(deleted_at__isnull=True)
+        return super().get_queryset().exclude(deleted_at__isnull=False)
 
 
-class DefaultManager(ExcludeDeletedManagerMixin, models.Manager):
-    pass
+class SoftDeleteModelMixin:
+    """
+    mixin for soft deleted any Model
+    expected that this mixin will be applied with a SoftDeletedManager
+    """
+    deleted_at = models.DateTimeField(null=True, default=None, db_index=True)
+
+    def soft_delete(self):
+        self.deleted_at = timezone.now()
+        self.save()
+
+    def restore(self):
+        self.deleted_at = None
+        self.save()
 
 
 class UUIDModel(models.Model):
+    """
+    default abstract model for custom models
+    """
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    deleted_at = models.DateTimeField(null=True)
 
-    objects = DefaultManager
+    class Meta:
+        abstract = True
+
+
+class SoftDeletedUUIDModel(SoftDeleteModelMixin, UUIDModel):
+    """
+    default abstract model with SoftDeleteModelMixin for custom models
+    """
+    objects = SoftDeletedManager
     all_objects = models.Manager
 
     class Meta:
